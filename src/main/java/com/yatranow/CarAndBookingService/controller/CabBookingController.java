990ce793dcdd789be8126ac3bd53b64b6815069f
javasoftarun <javasoftarun159@gmail.com>
2025-04-26 15:41:13 +0530

    cors enable

--- src/main/java/com/yatranow/CarAndBookingService/controller/CabBookingController.java
+++ src/main/java/com/yatranow/CarAndBookingService/controller/CabBookingController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/cab/booking")
 @Tag(name = "Cab Booking Controller", description = "Endpoints for managing cab bookings")
