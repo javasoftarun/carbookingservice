58ea9a6e8d297a31588fed91da1cb772d18f4df3
javasoftarun <javasoftarun159@gmail.com>
2025-05-11 13:20:16 +0530

    search api changes

--- src/main/java/com/yatranow/CarAndBookingService/service/CabService.java
+++ src/main/java/com/yatranow/CarAndBookingService/service/CabService.java
@@ -3,18 +3,17 @@ package com.yatranow.CarAndBookingService.service;
 import java.util.List;
 import java.util.logging.Logger;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yatranow.CarAndBookingService.repository.CabRegistrationDetailsRepository;
 import com.yatranow.CarAndBookingService.request.CabSearchRequest;
 import com.yatranow.CarAndBookingService.response.CabSearchResponse;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CabService {
@@ -31,14 +30,6 @@ public class CabService {
         return cabDetailsRepository.findAvailableCabs(cabSearchRequest.getPickupDateTime()).stream()
                 .map(cab -> {
                     CabSearchResponse cabSearchResponse = new CabSearchResponse();
-//                    double pickupLat = Double.parseDouble(cabSearchRequest.getPickupLat());
-//                    double pickupLng = Double.parseDouble(cabSearchRequest.getPickuplng());
-//                    double dropLat = Double.parseDouble(cabSearchRequest.getDropLat());
-//                    double dropLng = Double.parseDouble(cabSearchRequest.getDropLng());
-//                    double cabLat = Double.parseDouble(cab.getLatitude());
-//                    double cabLng = Double.parseDouble(cab.getLongitude());
-
-                    //double distanceToPickup = calculateDistance(pickupLat, pickupLng, cabLat, cabLng);
 					double distanceToPickup = 0;
 					try {
 						distanceToPickup = findDistance(cab.getAddress(), cabSearchRequest.getPickupLocation());
@@ -46,9 +37,7 @@ public class CabService {
 						throw new RuntimeException(e);
 					}
 
-
 					if (distanceToPickup <= cabSearchRequest.getRadius()) {
-                        //	double distanceToDrop = calculateDistance(dropLat, dropLng, pickupLat, pickupLng);
 						double distanceToDrop = 0;
 						try {
 							distanceToDrop = findDistance(cabSearchRequest.getPickupLocation(), cabSearchRequest.getDropLocation());
@@ -83,18 +72,6 @@ public class CabService {
         double fare = baseFare + (totalDistance * 2 * perKmRate);
         return fare;
     }
-
-//    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-//        // Haversine formula to calculate distance between two points
-//        final int R = 6371; // Radius of the Earth in km
-//        double latDistance = Math.toRadians(lat2 - lat1);
-//        double lonDistance = Math.toRadians(lon2 - lon1);
-//        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-//                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-//                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-//        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-//        return R * c; // Distance in km
-//    }
 
     private double findDistance(String fromLocation, String toLocation) throws JsonProcessingException {
 		logger.info("In CabService.findDistance(-) : Finding distance using Google Maps API");
